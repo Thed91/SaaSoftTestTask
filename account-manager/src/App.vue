345b68e0e1fe3b2061b2763b10426f2e3f40f675
Fiodar Mikhadziuk <f.mikhadziuk@gmail.com>
2026-02-06 13:10:01 +0400

    first commit

---
+++ account-manager/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AccountForm from './components/AccountForm.vue'
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <AccountForm />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
